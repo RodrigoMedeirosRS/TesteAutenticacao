9fb61f1036ae4ee3a690710884e6a91c44c85ed8
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-03-26 11:16:36 -0300

    feat(cliente de text)

---
+++ Teste.Autenticacao/PROG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+using DTO;
+using BibliotecaViva.SAL;
+
+namespace PROG
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var url = string.Empty;
+            var requisicao = new Requisicao();
+            var retorno = requisicao.ExecutarPost<TextoDTO, TextoDTO>(url, ObterMensagemDeEnvio());
+            Console.WriteLine(retorno.Texto);
+        }
+        static TextoDTO ObterMensagemDeEnvio()
+        {
+            var mensagem = Console.ReadLine();
+            return new TextoDTO()
+            {
+                Texto = mensagem
+            };
+        }
+    }
+}
